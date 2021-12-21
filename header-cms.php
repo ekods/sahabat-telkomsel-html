c0f48d2c735f7bc421d73c306c281ae24039286e
F Luga <fernandus.luga@gmail.com>
2021-12-21 22:37:20 +0700

    Add Header & Page

---
+++ header-cms.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sahabat Telkomsel</title>
+
+    <meta name="description" content="Sahabat Telkomsel adalah jalan termudah bagi para content creator untuk terhubung dengan orang-orang dan mendapat banyak kesempatan untuk mengembangkan karirmu.">
+    <meta name="keywords" content="">
+    <meta name="author" content="Sahabat Telkomsel">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- Bootstrap CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- CSS Files For Plugin -->
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/font-awesome/font-awesome.min.css" rel="stylesheet">
+    <link href="css/magnific-popup.css" rel="stylesheet" />
+    <link href="css/YTPlayer.css" rel="stylesheet" />
+    <link href="inc/owlcarousel/css/owl.carousel.min.css" rel="stylesheet" />
+    <link href="inc/owlcarousel/css/owl.theme.default.min.css" rel="stylesheet" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css'>
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+
+
+  </head>
+  <body data-spy="scroll" data-target=".navbar-fixed-top" data-offset="100">
+      <!-- Start Navigation -->
+    <header class="nav-solid" id="home">
+
+        <nav class="navbar navbar-fixed-top">
+            <div class="navigation">
+                <div class="container-fluid">
+                    <div class="row">
+
+                        <div class="navbar-header">
+                            <!-- Logo -->
+                            <div class="logo-container">
+                                <div class="logo-wrap local-scroll">
+                                  <a href="index.php">
+                                    <img class="logo" src="img/logo.svg" alt="logo" data-rjs="2">
+                                  </a>
+                                </div>
+                            </div>
+                        </div> <!-- end navbar-header -->
+
+                        <div class="col-md-8 col-xs-12 nav-wrap">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="nav-item dropdown me-3">
+                                    <a href="">
+                                        <i class="fa fa-bell-o" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div> <!-- /.col -->
+
+                    </div> <!-- /.row -->
+                </div> <!--/.container -->
+            </div> <!-- /.navigation-overlay -->
+        </nav> <!-- /.navbar -->
+
+    </header>
+    <!-- End Navigation -->
---
+++ silver-dashboard.php
@@ -0,0 +1,3 @@
+<?php include('header-cms.php'); ?>
+
+<?php include('footer-1.php'); ?>
