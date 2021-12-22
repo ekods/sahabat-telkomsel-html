c51f711effe238293d00525637fc0de71d66c0a7
eko.dwi <44047301+ekods@users.noreply.github.com>
2021-12-22 11:43:59 +0700

    header dashboard user

---
+++ dashboard-index.php
@@ -0,0 +1,19 @@
+<?php include('header-2.php'); ?>
+
+  <section class="ptb-40">
+    <div class="container">
+
+      <div class="col-wrap">
+        <div class="col-left">
+          <?php include('dashboard-nav.php'); ?>
+        </div>
+
+        <div class="col-right">
+          ssss
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+<?php include('footer-1.php'); ?>
---
+++ header-2.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sahabat Telkomsel</title>
+
+    <meta name="description" content="Sahabat Telkomsel adalah jalan termudah bagi para content creator untuk terhubung dengan orang-orang dan mendapat banyak kesempatan untuk mengembangkan karirmu.">
+    <meta name="keywords" content="">
+    <meta name="author" content="Sahabat Telkomsel">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- Bootstrap CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- CSS Files For Plugin -->
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/font-awesome/font-awesome.min.css" rel="stylesheet">
+    <link href="css/magnific-popup.css" rel="stylesheet" />
+    <link href="css/YTPlayer.css" rel="stylesheet" />
+    <link href="inc/owlcarousel/css/owl.carousel.min.css" rel="stylesheet" />
+    <link href="inc/owlcarousel/css/owl.theme.default.min.css" rel="stylesheet" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+
+  </head>
+  <body data-spy="scroll" data-target=".navbar-fixed-top" data-offset="100">
+
+
+    <!-- Preloader -->
+    <!-- <div id="preloader">
+        <div id="spinner"></div>
+    </div> -->
+    <!-- End Preloader-->
+
+
+    <!-- Start Navigation -->
+    <header class="nav-solid" id="home">
+
+        <nav class="navbar navbar-fixed-top">
+            <div class="navigation">
+                <div class="container-fluid">
+                    <div class="row header-inner">
+
+                        <div class="navbar-header">
+                            <!-- Logo -->
+                            <div class="logo-container">
+                                <div class="logo-wrap local-scroll">
+                                  <a href="index.php">
+                                    <img class="logo" src="img/logo.svg" alt="logo" data-rjs="2">
+                                  </a>
+                                </div>
+                            </div>
+                        </div> <!-- end navbar-header -->
+
+                        <div class="col-md-8 col-xs-12 nav-wrap">
+                          <div class="flex-none flex w-40 px-2">
+                        			<div class="nav-right">
+                                <div class="nav-right-item">
+                                  <a href="#" class="">
+                                    <div class="notif-bell notif-in"></div>
+                                  </a>
+                                </div>
+                        				<div class="nav-right-item">
+                                  <div class="dropdown">
+                                    <a href="#" class="dropdown-toggle"  data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                      <div class="avatar-h">
+                                        <img src="img/st/dummy/254a838cf18e359b6692764d3eae4ec1.png" alt="">
+                                      </div>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                      <li>
+                                        <a href="#">
+                                          <p>Account</p>
+                                        </a>
+                                      </li>
+                                      <li>
+                                        <a href="#">
+                                          <p>Logout</p>
+                                        </a>
+                                      </li>
+                                    </ul>
+                                  </div>
+                        				</div>
+                        			</div>
+                        		</div>
+                        </div> <!-- /.col -->
+
+                    </div> <!-- /.row -->
+                </div> <!--/.container -->
+            </div> <!-- /.navigation-overlay -->
+        </nav> <!-- /.navbar -->
+
+    </header>
+    <!-- End Navigation -->
