10f85138a424b85a663e2e5ab31fbea084d202ba
F Luga <fernandus.luga@gmail.com>
2021-12-28 13:36:20 +0700

    Add Pengaturan & Security

---
+++ pengaturan.php
@@ -0,0 +1,122 @@
+<?php include('header-2.php'); ?>
+
+  <section>
+    <div class="container">
+      <div class="alert alert-primary mt-3 mb-3 bgblueOld" role="alert">
+        <h3 class="mb-0"><b>Akunmu Sedang Dalam Verifikasi</b></h3>
+        <p>Tim admin kami sedang melakukan verifikasi akunmu. Mohon menunggu sampai akunmu aktif dan bisa digunakan</p>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+      <div class="col-wrap"> 
+        <div class="col-left">
+          <?php include('dashboard-nav.php'); ?>
+        </div>
+
+            <div class="col-right">
+                <section>
+                    <div class="container">
+                        <h1 class="bold">Akun Saya</h1>
+                    </div>
+        
+                    <div class="row justify-content-between mb-5"> 
+                        <div class="col-md-6 my-2">
+                            <div class="dark-shadow">
+                                <a href="http://localhost:8888/sahabat-telkomsel-html/security.php" target="_blank">
+                                    <div class="row p-4">
+                                        <div class="col-md-3">
+                                            <div class="">
+                                                <img src="img/st/protect-icon.svg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="p-top-10">
+                                                <h4 class="bold">Login & Keamanan</h4>
+                                                <p class="neutral-40 mb-0">Ganti password dan tingkatkan keamanan akunmu</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 my-2">
+                            <div class="dark-shadow">
+                                <a href="#" target="_blank">
+                                <div class="row p-4">
+                                    <div class="col-md-3">
+                                        <div class="">
+                                            <img src="img/st/wallet-icon.svg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="p-top-10">
+                                            <h4 class="bold">Keuangan</h4>
+                                            <p class="neutral-40 mb-0">Kelola akun bank dan riwayat transaksi kamu</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <div class="container">
+                        <h1 class="bold">Dukungan</h1>
+                    </div>
+        
+                    <div class="row justify-content-between mb-5"> 
+                        <div class="col-md-6 my-2">
+                            <div class="dark-shadow">
+                                <a href="#" target="_blank">
+                                <div class="row p-4">
+                                    <div class="col-md-3">
+                                        <div class="">
+                                            <img src="img/st/faq-icon.svg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="p-top-10">
+                                            <h4 class="bold">FAQ</h4>
+                                            <p class="neutral-40 mb-0">Cari tahu informasi mengenai fitur dan layanan yang disediakan Sahabat Telkomsel untukmu</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 my-2">
+                            <div class="dark-shadow">
+                                <a href="#" target="_blank">
+                                <div class="row p-4">
+                                    <div class="col-md-3">
+                                        <div class="">
+                                            <img src="img/st/consultation-icon.svg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="p-top-10">
+                                            <h4 class="bold">Hubungi Customer Service</h4>
+                                            <p class="neutral-40 mb-0">Hubungi tim bantuan kami mengenai pertanyaan atau kendala yang kamu alami</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php include('footer-1.php'); ?>
---
+++ security.php
@@ -0,0 +1,94 @@
+<?php include('header-2.php'); ?>
+
+  <section>
+    <div class="container">
+      <div class="alert alert-primary mt-3 mb-3 bgblueOld" role="alert">
+        <h3 class="mb-0"><b>Akunmu Sedang Dalam Verifikasi</b></h3>
+        <p>Tim admin kami sedang melakukan verifikasi akunmu. Mohon menunggu sampai akunmu aktif dan bisa digunakan</p>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+      <div class="col-wrap"> 
+        <div class="col-left">
+          <?php include('dashboard-nav.php'); ?>
+        </div>
+
+            <div class="col-right">
+                <section>
+                    <div class="container">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="pengaturan.php">Pengaturan</a></li>
+                                <li class="breadcrumb-item">Akun Saya</li>
+                                <li class="breadcrumb-item active" aria-current="page">Login & Keamanan</li>
+                            </ol>
+                        </nav>
+                    </div>
+
+                    <section>
+                        <div class="container">
+                            <h1 class="bold">Login</h1>
+                        </div>
+            
+                        <div class="row justify-content-between mb-5"> 
+                            <div class="col-md-12">
+                    
+                                <div class="row">
+                                    <div class="col-md-8 d-flex align-items-center">
+                                        <div class="p-top-10 p-2">
+                                            <h3 class="bold">Password</h3>
+                                            <p class="neutral-40">Diperbarui 10 hari yang lalu</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center">
+                                        <a href="javascript:void(0);" class="bold fred offpage-nav-login">Ganti Password</a>
+                                    </div>
+
+                                    <div class="col-md-10">
+                                        <hr class="mt-2">
+                                    </div>
+
+                                </div>                            
+                        
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="container">
+                            <h1 class="bold">Akun</h1>
+                        </div>
+            
+                        <div class="row justify-content-between mb-5"> 
+                            <div class="col-md-12">
+                    
+                                <div class="row">
+                                    <div class="col-md-8 d-flex align-items-center">
+                                        <div class="p-top-10 p-2">
+                                            <h3 class="bold">Nonaktifkan Akun</h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center">
+                                        <a href="javascript:void(0);" class="bold fred offpage-nav-login">Ganti Password</a>
+                                    </div>
+
+                                </div>                            
+                        
+                            </div>
+                        </div>
+                    </section>
+                </section>
+
+
+                
+
+            </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php include('footer-1.php'); ?>
