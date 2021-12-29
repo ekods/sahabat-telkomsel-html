4b2ed096b49e9dd1d05278a0b16753fcc2649cde
F Luga <fernandus.luga@gmail.com>
2021-12-30 01:00:56 +0700

    Add Keuangan Page

---
+++ keuangan.php
@@ -0,0 +1,173 @@
+<?php include('header-2.php'); ?>
+
+  <section>
+    <div class="container">
+      <div class="alert alert-primary mt-3 mb-3 bgblueOld" role="alert">
+        <h3 class="mb-0"><b>Akunmu Sedang Dalam Verifikasi</b></h3>
+        <p>Tim admin kami sedang melakukan verifikasi akunmu. Mohon menunggu sampai akunmu aktif dan bisa digunakan</p>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+      <div class="col-wrap"> 
+        <div class="col-left">
+          <?php include('dashboard-nav.php'); ?>
+        </div>
+
+            <div class="col-right">
+                <section>
+                    <div class="container">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="pengaturan.php">Pengaturan</a></li>
+                                <li class="breadcrumb-item">Akun Saya</li>
+                                <li class="breadcrumb-item active" aria-current="page">Keuangan</li>
+                            </ol>
+                        </nav>
+                    </div>
+
+                    <section>
+                        <div class="container">
+                            <h1 class="bold">Keuangan</h1>
+                        </div>
+            
+                        <div class="row justify-content-between mb-5 pl-2"> 
+                            <div class="col-md-12">
+                    
+                                <div class="row">
+                                    <div class="col-md-8 d-flex align-items-center">
+                                        <div class="p-top-10 p-2">
+                                            <h3 class="bold">Akun Bank Saya</h3>
+                                            <p class="neutral-40">Diperbarui 10 hari yang lalu</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center">
+                                        <a href="javascript:void(0);" class="bold fred offpage-nav-login">Tambah Akun</a>
+                                    </div>
+
+                                    <div class="col-10 my-2">
+                                        <div class="dark-shadow">
+                                            <a href="http://localhost:8888/sahabat-telkomsel-html/security.php" target="_blank">
+                                                <div class="row p-4">
+                                                    <div class="col-auto d-flex align-items-center">
+                                                        <div class="">
+                                                            <img src="img/st/debit-icon.svg" alt="">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-8 d-flex align-items-center">
+                                                        <div class="p-top-10">
+                                                            <h4 class="bold mt-0 d-inline-block">50035281934926</h4>
+                                                            <div class="btn btn-default bold tag d-inline-block neutral-40">PRIMARY</div>
+                                                            <div>
+                                                                <p class="neutral-40 mb-0 d-inline-block">PT Bank Central Asia</p>
+                                                                <p class="d-inline-block">•</p>
+                                                                <p class="neutral-40 mb-0 d-inline-block">Maudy Ayunda Putri</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-2 d-flex align-items-center">
+                                                        <div class="row">
+                                                            <div class="col-6"><img src="img/st/trash-icon.svg" alt=""></div>
+                                                            <div class="col-6"><img src="img/st/black-edit-icon.svg" alt=""></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-10 my-2">
+                                        <div class="dark-shadow">
+                                            <a href="http://localhost:8888/sahabat-telkomsel-html/security.php" target="_blank">
+                                                <div class="row p-4">
+                                                    <div class="col-auto d-flex align-items-center">
+                                                        <div class="">
+                                                            <img src="img/st/debit-icon.svg" alt="">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-8 d-flex align-items-center">
+                                                        <div class="p-top-10">
+                                                            <h4 class="bold mt-0">50035281934926</h4>
+                                                            <p class="neutral-40 mb-0 d-inline-block">PT Bank Central Asia</p>
+                                                            <p class="d-inline-block">•</p>
+                                                            <p class="neutral-40 mb-0 d-inline-block">Maudy Ayunda Putri</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-2 d-flex align-items-center">
+                                                        <div class="row">
+                                                            <div class="col-6"><img src="img/st/trash-icon.svg" alt=""></div>
+                                                            <div class="col-6"><img src="img/st/black-edit-icon.svg" alt=""></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                </div>                            
+                        
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="container">
+                            <h1 class="bold">Transaksi</h1>
+                        </div>
+            
+                        <div class="pl-3 transaksi-table">
+                            <table class="table">
+                                <thead class="">
+                                    <tr>
+                                        <th class="text-left">Tgl Dibuat</th>
+                                        <th class="text-left">Tgl Diperbarui</th>
+                                        <th class="text-left">Challenge</th>
+                                        <th class="text-left">Keterangan</th>
+                                        <th class="text-left">Nominal</th>
+                                        <th class="text-left">Akun Bank </th>
+                                        <th class="text-left">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>16  November 2021 08:20:24</td>
+                                        <td>16  November 2021 08:20:24</td>
+                                        <td>Mola EPL Leicester City VS Chelsea (2021-11-20)</td>
+                                        <td>Mola EPL Leicester City VS Chelsea (2021-11-20)</td>
+                                        <td>Rp 3.500.000</td>
+                                        <td>50035281934926</td>
+                                        <td class="text-left">
+                                            <div class="btn btn-success">Diterima</div>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>16  November 2021 08:20:24</td>
+                                        <td>16  November 2021 08:20:24</td>
+                                        <td>Mola EPL Leicester City VS Chelsea (2021-11-20)</td>
+                                        <td>Mola EPL Leicester City VS Chelsea (2021-11-20)</td>
+                                        <td>Rp 3.500.000</td>
+                                        <td>50035281934926</td>
+                                        <td class="text-left">
+                                            <div class="btn btn-warning text-white">Menunggu</div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </section>
+                </section>
+
+
+                
+
+            </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php include('footer-1.php'); ?>
--- security.php
+++ security.php
@@ -33,7 +33,7 @@
                             <h1 class="bold">Login</h1>
                         </div>
             
-                        <div class="row justify-content-between mb-5"> 
+                        <div class="row justify-content-between mb-5 pl-2"> 
                             <div class="col-md-12">
                     
                                 <div class="row">
@@ -47,8 +47,33 @@
                                         <a href="javascript:void(0);" class="bold fred offpage-nav-login">Ganti Password</a>
                                     </div>
 
+                                    <div class="col-12">
+                                        <div class="contact-form row px-2">
+                                            <form class="form" action="" method="post">
+                                                <div class="col-sm-12 contact-form-item">
+                                                    <label for="">Password <span>*</span></label>
+                                                    <input name="password" id="password" type="password" placeholder="">
+                                                </div>
+                                                <a href="http://localhost:8888/sahabat-telkomsel-html/account-recovery.php" class="bold fred offpage-nav-login ml-3 pt-2">Lupa Password</a>
+
+                                                <div class="row pl-3 mt-5">
+                                                    <div class="col-md-12 col-lg-6">
+                                                        <label for="">Password Baru <span>*</span></label>
+                                                        <input name="password-baru" id="password-baru" type="password" placeholder="">
+                                                    </div>
+                                                    <div class="col-md-12 col-lg-6">
+                                                        <label for="">Konfirmasi Password Baru <span>*</span></label>
+                                                        <input name="konfirmasi-password-baru" id="konfirmasi-password-baru" type="password" placeholder="">
+                                                    </div>
+                                                </div>
+
+                                                <div class="clear"></div>
+                                            </form>
+                                        </div>
+                                    </div>
+
                                     <div class="col-md-10">
-                                        <hr class="mt-2">
+                                        <hr class="mt-5">
                                     </div>
 
                                 </div>                            
@@ -72,7 +97,7 @@
                                         </div>
                                     </div>
                                     <div class="col-auto d-flex align-items-center">
-                                        <a href="javascript:void(0);" class="bold fred offpage-nav-login">Ganti Password</a>
+                                        <a href="javascript:void(0);" class="bold fred offpage-nav-login">Nonaktifkan</a>
                                     </div>
 
                                 </div>                            
